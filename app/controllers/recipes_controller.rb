dc594d7cd80e83cd084532fd9e987ef6ac79f3f0
qh53 <qh53@cornell.edu>
2018-01-19 01:29:43 -0500

    styles: fixed header

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,15 +2,15 @@ class RecipesController < ApplicationController
   
   def index
   	@to_search = params[:search] || "chocolate"
-  	@page = params[:page] || 1
-  	@page = 1 if @page.to_i <= 0 
+  	@page = params[:page] || '1'
+  	@page = '1' if @page.to_i <= 0 
   	@result = Recipe.for @to_search, @page
   end
 
   def pretty
   	@to_search = params[:search] || "chocolate"
-  	@page = params[:page] || 1
-  	@page = 1 if @page.to_i <= 0 
+  	@page = params[:page] || '1'
+  	@page = '1' if @page.to_i <= 0 
   	@result = Recipe.for @to_search, @page
   end
 
